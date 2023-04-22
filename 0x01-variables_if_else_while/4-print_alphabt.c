e269273ae19807d7b3cd8413c3e5fa05101cee14
Uche-Viv <Eluoguuv15069@gmail.com>
2023-04-22 07:14:06 -0700

    4-print_alphabt.c: In function ‘main’:

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+
+	if (c != 'e' && c != 'q')
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
