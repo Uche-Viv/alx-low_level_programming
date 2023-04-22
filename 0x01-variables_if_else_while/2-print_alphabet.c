247976cf2cd8715416b3fb399f681c0b025f47ca
Uche-Viv <Eluoguuv15069@gmail.com>
2023-04-22 03:04:26 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,15 @@
  *
  * Return: Always 0 (success)
  */
-
 int main(void)
 {
-int lowerCase = 'a';
-while (lowerCase <= 'z')
-}
-putchar(lowerCase);
-lowerCase += 1;
-}
-putchar('\n');
-return (0);
+	int lowerCase = 'a';
+
+	while (lowerCase <= 'z')
+	{
+		putchar(lowerCase);
+		lowerCase += 1;
+	}
+	putchar('\n');
+	return (0);
 }
