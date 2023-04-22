8b4fb1ab4ff2e748ec907defc459ae6b96d8e3b8
Uche-Viv <Eluoguuv15069@gmail.com>
2023-04-22 08:41:30 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints the lowercase alphabet in reverse
+ * main - Prints all the numbers of base 16 in lowercase
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
+	int num;
 	char letter;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
 
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
 	putchar('\n');
 
 	return (0);
